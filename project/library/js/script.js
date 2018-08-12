58157ac3bdcf095352fc08b377b0ded7714cfc80
joseafs <joseafs@outlook.com.br>
2018-08-12 20:21:40 -0300

    estrutura base com tabela - v-model

--- project/library/js/script.js
+++ project/library/js/script.js
@@ -1,3 +1,61 @@
+vm = new Vue({
+    el: '#pedido',
+    data: {
+        pizza: {
+            tamanho: '',
+            sabor: '',
+            adicional: {},
+        },
+        config: {
+            titulo: 'Pizzaria UDS',
+            CNPJ: '...',
+            endereco: 'Nildo Ribeiro'
+        },
+        selected: 'A',
+        
+        page: {
+            msg_1: '',
+            msg_2: 'Quer deixar sua pizza mais interessante ? <br />basta se aventurar nas opções abaixo !',
+        },
+        teste: 'Hello Vue.js!',
+        tamanho: [
+            {id: 0, titulo: 'Pequena', valor: 'R$ 20,00', tempo: '15'},
+            {id: 1, titulo: 'Média', valor: 'R$ 30,00', tempo: '20'},
+            {id: 2, titulo: 'Grande', valor: 'R$ 40,00', tempo: '25'}
+        ],
+        sabor: [
+            {id: 0, titulo: 'Calabresa', valor: '', tempo: ''},
+            {id: 1, titulo: 'Marguerita', valor: '', tempo: ''},
+            {id: 2, titulo: 'Portuguesa', valor: '', tempo: '5'}
+        ],
+        adicional: [
+            {id: 0, titulo: 'Bacon', valor: '3,00', tempo: '', qtd: 0, opt: '',max_qtd: '10'},
+            {id: 1, titulo: 'Cebola', valor: '', tempo: '',  qtd: 0, opt: ['Sim','Não'], max_qtd: '1'},
+            {id: 2, titulo: 'Borda Recheada', valor: '5,00', tempo: '5', qtd: 0, opt: '', max_qtd: '2'}
+        ],
+        msg_max: 'Wow, chegou no limite de incrementos que pode adicionar, iremos colocar um adicional de brinde, hehehe !',
+        vl_total: 10
+    },
+    methods: {
+        low_string: function () {
+            // info = this.el = this.el.split(' ').join('_').toLowerCase();
+            console.log('passou');
+            // return info;
+        },
+        count_vt: function (v) {
+            // info = this.el = this.el.split(' ').join('_').toLowerCase();
+            console.log(this.vl_total);
+            // return info;
+        },
+        save: function() {
+            console.log(this.pizza);
+            console.log(this.pizza.tamanho);
+            console.log('salvo');
+        }
+
+    }
+})
+
 /* -------------------------------------------------------------------------- */
 /* Mask */
 // $(".data").mask("99/99/9999");
